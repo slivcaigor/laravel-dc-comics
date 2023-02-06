a0aaa25f1ae54a4e4f90a6920127f8ff95928c69
slivcaigor <slivcaigor@gmail.com>
2023-02-06 14:41:54 +0100

    Database Migration

---
+++ database/migrations/2023_02_06_132937_create_humans_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('humans', function (Blueprint $table) {
+            $table->string('firstName', 32);
+            $table->string('lastName', 32);
+            $table->date('dateOfBirth');
+            $table->integer('heigth') -> nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('humans');
+    }
+};
